449d94db9714ddf807da8974cc63da4a6349c065
Shaun Fong <ghost_h@foxmail.com>
2024-06-07 21:01:31 +0800

    Update Make.sh

--- Make.sh
+++ Make.sh
@@ -1,33 +1,31 @@
 #!/usr/bin/env bash
-Version=3.1.4
-
-BuildSourcePackage="github.com/davyxu/tabtoy/build"
-BinaryPackage="github.com/davyxu/tabtoy"
+BuildSourcePackage="github.com/shaunfong/tabtoy/build"
+BinaryPackage="github.com/shaunfong/tabtoy"
 BinaryName="tabtoy"
 
 BuildBinary()
 {
   set -e
-  TargetDir=bin/"${1}"
+  TargetDir=bin/"${2}"
   mkdir -p "${TargetDir}"
-  export GOOS=${1}
+  export GOOS=${2}
   BuildTime=$(date -R)
   GitCommit=$(git rev-parse HEAD)
-  VersionString="-X \"${BuildSourcePackage}.BuildTime=${BuildTime}\" -X \"${BuildSourcePackage}.Version=${Version}\" -X \"${BuildSourcePackage}.GitCommit=${GitCommit}\""
+  VersionString="-X \"${BuildSourcePackage}.BuildTime=${BuildTime}\" -X \"${BuildSourcePackage}.Version=${1}\" -X \"${BuildSourcePackage}.GitCommit=${GitCommit}\""
 
   go build -v -p 4 -o "${TargetDir}"/${BinaryName} -ldflags "${VersionString}" ${BinaryPackage}
   PackageDir=$(pwd)
   cd "${TargetDir}"
-  tar zcvf "${PackageDir}"/${BinaryName}-${Version}-"${1}"-x86_64.tar.gz ${BinaryName}
+  tar zcvf "${PackageDir}"/${BinaryName}-${1}-"${2}"-x86_64.tar.gz ${BinaryName}
   cd "${PackageDir}"
 }
 
 
-if [[ ${1} == "" ]]; then
+if [[ ${2} == "" ]]; then
   BuildBinary windows
   BuildBinary linux
   BuildBinary darwin
 else
-  BuildBinary "${1}"
+  BuildBinary "${2}"
 fi
 
